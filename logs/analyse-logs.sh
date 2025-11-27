9767757c06a4c0ffd7e2ab501f44dd20ea66bbb7
Aleksandar Ristic <storiaca@gmail.com>
2025-11-27 12:24:25 +0100

    Add new line to echo

--- logs/analyse-logs.sh
+++ logs/analyse-logs.sh
@@ -3,23 +3,23 @@
 echo "Analysing log files"
 echo "==================="
 
-echo "List of log files updated in last 24 hours"
+echo -e "\nList of log files updated in last 24 hours"
 find . -name “*.log” -mtime -1
 
-echo "Searching ERROR logs in application.log file" 
+echo -e "\nSearching ERROR logs in application.log file" 
 grep "ERROR" application.log
 
-echo "Number of ERROR logs found in application.log"
+echo -e "\nNumber of ERROR logs found in application.log"
 grep -c "ERROR" application.log
 
-echo "Number of FATAL logs found in application.log" 
+echo -e "\nNumber of FATAL logs found in application.log" 
 grep -c "FATAL" application.log
 
-echo "Number of FATAL logs found in system.log"
+echo -e "\nNumber of FATAL logs found in system.log"
 grep -c "FATAL" system.log
 
-echo "Number of CRITICAL logs found in system.log"
+echo -e "\nNumber of CRITICAL logs found in system.log"
 grep -c "CRITICAL" system.log
 
-echo "CRITICAL logs found in system.log"
+echo -e "\nCRITICAL logs found in system.log"
 grep "CRITICAL" system.log
