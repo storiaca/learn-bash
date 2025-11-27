d32a41758378ddd5a5e20ebbe21f7289ef972cd2
Aleksandar Ristic <storiaca@gmail.com>
2025-11-27 12:59:49 +0100

    Change paths for log files

--- analyse-logs.sh
+++ analyse-logs.sh
@@ -4,22 +4,22 @@ echo "Analysing log files"
 echo "==================="
 
 echo -e "\nList of log files updated in last 24 hours"
-find . -name “*.log” -mtime -1
+find /home/aca/learn-bash/logs -name “*.log” -mtime -1
 
 echo -e "\nSearching ERROR logs in application.log file" 
-grep "ERROR" application.log
+grep "ERROR" /home/aca/learn-bash/logs/application.log
 
 echo -e "\nNumber of ERROR logs found in application.log"
-grep -c "ERROR" application.log
+grep -c "ERROR" /home/aca/learn-bash/logs/application.log
 
 echo -e "\nNumber of FATAL logs found in application.log" 
-grep -c "FATAL" application.log
+grep -c "FATAL" /home/aca/learn-bash/logs/application.log
 
 echo -e "\nNumber of FATAL logs found in system.log"
-grep -c "FATAL" system.log
+grep -c "FATAL" /home/aca/learn-bash/logs/system.log
 
 echo -e "\nNumber of CRITICAL logs found in system.log"
-grep -c "CRITICAL" system.log
+grep -c "CRITICAL" /home/aca/learn-bash/logs/system.log
 
 echo -e "\nCRITICAL logs found in system.log"
-grep "CRITICAL" system.log
+grep "CRITICAL" /home/aca/learn-bash/logs/system.log
