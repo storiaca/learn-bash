924bd43ed3bba5f02b3676be06c15b9dc8212159
Aleksandar Ristic <storiaca@gmail.com>
2025-11-28 15:00:02 +0100

    Arrays in shell script

--- analyse-logs.sh
+++ analyse-logs.sh
@@ -4,26 +4,29 @@ LOG_DIR="/home/aca/learn-bash/logs"
 APP_LOG_FILE="application.log"
 SYS_LOG_FILE="system.log"
 
+ERROR_PATTERNS=("ERROR" "FATAL" "CRITICAL")
+
 echo "Analysing log files"
 echo "==================="
 
 echo -e "\nList of log files updated in last 24 hours"
-find $LOG_DIR -name “*.log” -mtime -1
+LOG_FILES=$(find $LOG_DIR -name “*.log” -mtime -1)
+echo "$LOG_FILES"
 
 echo -e "\nSearching ERROR logs in application.log file" 
-grep "ERROR" "$LOG_DIR/$APP_LOG_FILE"
+grep "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
 
 echo -e "\nNumber of ERROR logs found in application.log"
-grep -c "ERROR" "$LOG_DIR/$APP_LOG_FILE"
+grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
 
 echo -e "\nNumber of FATAL logs found in application.log" 
-grep -c "FATAL" "$LOG_DIR/$APP_LOG_FILE"
+grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$APP_LOG_FILE"
 
 echo -e "\nNumber of FATAL logs found in system.log"
-grep -c "FATAL" "$LOG_DIR/$SYS_LOG_FILE"
+grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$SYS_LOG_FILE"
 
 echo -e "\nNumber of CRITICAL logs found in system.log"
-grep -c "CRITICAL" "$LOG_DIR/$SYS_LOG_FILE"
+grep -c "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
 
 echo -e "\nCRITICAL logs found in system.log"
-grep "CRITICAL" "$LOG_DIR/$SYS_LOG_FILE"
+grep "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
