2e691e45379e150baa3006fc0a2697866d05f808
Aleksandar Ristic <storiaca@gmail.com>
2025-11-30 18:20:54 +0100

    Adding loop in shell script

--- analyse-logs.sh
+++ analyse-logs.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 LOG_DIR="/home/aca/learn-bash/logs"
-APP_LOG_FILE="application.log"
-SYS_LOG_FILE="system.log"
 
 ERROR_PATTERNS=("ERROR" "FATAL" "CRITICAL")
 
@@ -10,23 +8,25 @@ echo "Analysing log files"
 echo "==================="
 
 echo -e "\nList of log files updated in last 24 hours"
-LOG_FILES=$(find $LOG_DIR -name “*.log” -mtime -1)
+LOG_FILES=$(find "$LOG_DIR" -name "*.log" -mtime -1)
 echo "$LOG_FILES"
 
-echo -e "\nSearching ERROR logs in application.log file" 
-grep "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
+for LOG_FILE in $LOG_FILES; do
+  echo -e "\nSearching ${ERROR_PATTERNS[0]} logs in $LOG_FILE file" 
+  grep "${ERROR_PATTERNS[0]}" "$LOG_FILE"
 
-echo -e "\nNumber of ERROR logs found in application.log"
-grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
+  echo -e "\nNumber of ${ERROR_PATTERNS[0]} logs found in $LOG_FILE"
+  grep -c "${ERROR_PATTERNS[0]}" "$LOG_FILE"
 
-echo -e "\nNumber of FATAL logs found in application.log" 
-grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$APP_LOG_FILE"
+  echo -e "\nSearching ${ERROR_PATTERNS[1]} logs in $LOG_FILE file" 
+  grep "${ERROR_PATTERNS[1]}" "$LOG_FILE"
 
-echo -e "\nNumber of FATAL logs found in system.log"
-grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$SYS_LOG_FILE"
+  echo -e "\nNumber of ${ERROR_PATTERNS[1]} logs found in $LOG_FILE" 
+  grep -c "${ERROR_PATTERNS[1]}" "$LOG_FILE"
 
-echo -e "\nNumber of CRITICAL logs found in system.log"
-grep -c "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
+  echo -e "\nSearching ${ERROR_PATTERNS[2]} logs in $LOG_FILE file" 
+  grep "${ERROR_PATTERNS[2]}" "$LOG_FILE"
 
-echo -e "\nCRITICAL logs found in system.log"
-grep "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
+  echo -e "\nNumber of ${ERROR_PATTERNS[2]} logs found in $LOG_FILE"
+  grep -c "${ERROR_PATTERNS[2]}" "$LOG_FILE"
+done
